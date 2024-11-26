0944dd817ddc32c295af5e6d7c95f531dcabb7b9
Fabio Lopes <fabiolopes7844@gmail.com>
2024-11-25 23:10:00 -0300

    Refactor testeP2 function to improve readability, add testing for SalaDeReuniao and SalaDeTrabalhoCompartilhada, and enhance exception handling

--- testeP2.cpp
+++ testeP2.cpp
@@ -7,34 +7,75 @@
 
 using namespace std;
 
-void testeP2() {
+void teste()
+{
+    SalaDeReuniao *sala = new SalaDeReuniao("oi", 10);
+    // cout << sala->getNome();
+    sala->imprimir();
+}
+
+void testeP2()
+{
     // Crie uma sala de reuniao com nome "A1" e 100m2,
     // colocando-a numa variavel do tipo Sala e a imprima
+    try
+    {
+        Sala *sala = new SalaDeReuniao("A1", 100);
+        sala->imprimir();
+        // Altere o valor do preco por metro quadrado para 15 das salas de reuniao
 
-    // Altere o valor do preco por metro quadrado para 15 das salas de reuniao
+        dynamic_cast<SalaDeReuniao *>(sala)->setPrecoPorMetroQuadrado(15);
 
-    // imprima novamente a sala de reuniao com nome "A1"
+        // imprima novamente a sala de reuniao com nome "A1"
 
-    // Crie uma sala de trabalho com nome "A2" e 100m2,
-    // colocando-a numa variavel do tipo Sala
+        sala->imprimir();
 
-    // Adicione as seguintes mesas a sala A2
-    // Mesa com id 1 e tamanho de 10m2
+        // Crie uma sala de trabalho com nome "A2" e 100m2,
+        // colocando-a numa variavel do tipo Sala
 
-    // Mesa com id 2 e tamanho de 12m2
+        Sala *salaTrabalho = new SalaDeTrabalho("A2", 100);
 
-    // Imprima a sala de trabalho com nome A2
+        // Adicione as seguintes mesas a sala A2
+        // Mesa com id 1 e tamanho de 10m2
+        Mesa *m = new Mesa(1, 10);
+        dynamic_cast<SalaDeTrabalho *>(salaTrabalho)->adicionar(m);
 
-    // Crie uma sala de trabalho compartilhada com nome "A3" e 30m2,
-    // colocando-a em uma variável do tipo SalaDeTrabalhoCompartilhada
+        // Mesa com id 2 e tamanho de 12m2
+        Mesa *m2 = new Mesa(2, 120);
+        dynamic_cast<SalaDeTrabalho *>(salaTrabalho)->adicionar(m2);
 
-    // Adicione as seguintes mesas:
-    // Mesa com id 3 e tamanho de 10m2
+        // Imprima a sala de trabalho com nome A2
+        salaTrabalho->imprimir();
 
-    // Mesa com id 4 e tamanho de 12m2
+        // Crie uma sala de trabalho compartilhada com nome "A3" e 30m2,
+        // colocando-a em uma variï¿½vel do tipo SalaDeTrabalhoCompartilhada
 
-    //  Reserve somente a mesa com id 4 e imprima a sala de trabalho com nome A3.
+        SalaDeTrabalhoCompartilhada *salaTrabalhoCompartilhada = new SalaDeTrabalhoCompartilhada("A3", 30);
 
-    //Destrua todas as salas
+        // Adicione as seguintes mesas:
+        // Mesa com id 3 e tamanho de 10m2
+        // Mesa com id 4 e tamanho de 12m2
 
+        Mesa *m3 = new Mesa(3, 10);
+        salaTrabalhoCompartilhada->adicionar(m3);
+        Mesa *m4 = new Mesa(4, 12);
+        salaTrabalhoCompartilhada->adicionar(m4);
+
+        //  Reserve somente a mesa com id 4 e imprima a sala de trabalho com nome A3.
+        salaTrabalhoCompartilhada->reservar(m4);
+
+        salaTrabalhoCompartilhada->imprimir();
+
+        delete sala;
+        delete salaTrabalho;
+        delete salaTrabalhoCompartilhada;
+
+    }catch(logic_error *e){
+        cout << e->what() << endl;
+    }
+    catch(invalid_argument *e){
+        cout << e->what() << endl;
+    }
+
+    // Destrua todas as salas
 }
